520ced012d9021e71641b30dd6d6bc04abda120b
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2023-11-04 11:39:25 +0000

    Deleted DB information

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,9 @@
 <?php
-// $conn = mysqli_connect('', '', '', '');
 
-$conn = mysqli_connect('localhost', 'test1', 'pokemoN12', 'tobi_pizza');
+//Add database details here
+$conn = mysqli_connect('', '', '', '');
 
+ 
 if (!$conn) {
     echo 'Connection error:' . mysqli_connect_error();
 }
